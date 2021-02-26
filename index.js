f196e36c2620aa9b607a8084570aa0900b571a41
trung <trung5897@gmail.com>
2021-02-26 23:31:26 +0700

    ok

--- index.js
+++ index.js
@@ -1,29 +1,14 @@
-const express = require('express')
-
-const app = express()
-
-const port = 3000
-
-const unirest = require("unirest");
-
-
-app.get('/', (req, res) => {
-    const apiCall = unirest(
-        "GET",
-        "https://ip-geolocation-ipwhois-io.p.rapidapi.com/json/"
-    );
-
-    apiCall.headers({
-        "x-rapidapi-host": "ip-geolocation-ipwhois-io.p.rapidapi.com",
-        "x-rapidapi-key": "ba00575e52msha07fd0c94b6f030p14c4f1jsn1b741c25cf74"
-    });
-
-    apiCall.end(function (result) {
-        if (res.error) throw new Error(result.error);
-        console.log(result.body);
-        res.send(result.body);
-    });
-})
-
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+var express = require('express'); 
+var app = express();  
+var PORT = 3000; 
+  
+app.enable('trust proxy') 
+app.get('/', function (req, res) { 
+  console.log(req.ip); 
+  res.send(); 
+}); 
+  
+app.listen(PORT, function(err){ 
+    if (err) console.log(err); 
+    console.log("Server listening on PORT", PORT); 
+});
